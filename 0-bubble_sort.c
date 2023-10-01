d2713f75ee4f804929f2e3e0f02f4a6436e93137
Allan Kirui <allancheruiyot003@gmail.com>
2023-10-01 23:28:46 +0300

    ALX SE

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,46 @@
 #include "sort.h"
 
 /**
-* bubble_sort - sorts integers in ascending order using the bubble sort algoritm
-* @array: array to be sorted
-* @size: size of the array
-*/
+ * swap - Swaps integers in the array
+ * @a: The first integer
+ * @b: The second integer
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers using the bubble sort algorithm
+ * @array: Represents the array
+ * @size: Size of the array.
+ *
+ * Description: Sorts an array in ascending order
+ */
 void bubble_sort(int *array, size_t size)
 {
-    int *temp;
-    size_t i, k;
+	size_t i, len = size;
+	int bubbly = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        for (k = i + 1; k <= size; k++)
-        {
-            if (array[i] > array[k])
-            {
-                temp[i] = array[k];
-                temp[k] = array[i];
-                continue;
-            }
-            print_array(temp, size);
-        }
-    }
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == 0)
+	{
+		bubbly = 1;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = 0;
+			}
+		}
+		len--;
+	}
 }
