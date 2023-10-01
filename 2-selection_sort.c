bfda68800a354b64989f3f1c1178858bae90255c
Allan Kirui <allancheruiyot003@gmail.com>
2023-10-02 00:02:40 +0300

    ALX SE

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers in an array
+ * @a: The first integer
+ * @b: The second integer
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * @array: Array of integers
+ * @size: Size of the array
+ *
+ * Description: Sorts an array of integers using the selection sort
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
